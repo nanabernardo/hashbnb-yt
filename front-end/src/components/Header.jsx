d6f73c516bb6ef200cbf44cd30522d6dc5756612
nanabernardo <nanabernardo@gmail.com>
2025-05-16 17:10:25 -0300

    feat: update Header with routing and responsive layout

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -1,19 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className="shadow-md">
-      <div className="mx-auto flex max-w-7xl items-center justify-between px-8 py-4">
-        <div className="flex items-center">
+      <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-4 sm:px-8">
+        <Link to="/" className="flex items-center">
           <img
             className="h-10"
             src="https://cdn.prod.website-files.com/61b9e0dd381626819c8d4f83/65e2198d48039ba6444f602b_logo%20hashtag%20-%20h.webp"
             alt="Logo da Hastag"
           />
           <p className="text-primary-400 text-2xl font-bold">ashbnb</p>
-        </div>
+        </Link>
 
-        <div className="flex items-center rounded-full border border-gray-300 py-2 pr-4 pl-6 shadow-md">
+        <Link
+          to="/"
+          className="hidden items-center rounded-full border border-gray-300 py-2 pr-4 pl-6 shadow-md lg:flex"
+        >
           <p className="border-r border-r-gray-300 pr-4">Qualquer lugar</p>
           <p className="border-r border-r-gray-300 px-4">Qualquer semana</p>
           <p className="px-4">Hóspedes</p>
@@ -34,9 +38,12 @@ const Header = () => {
               />
             </svg>
           </div>
-        </div>
+        </Link>
 
-        <div className="flex items-center gap-2 rounded-full border border-gray-300 py-2 pr-4 pl-6 shadow-md">
+        <Link
+          to="/login"
+          className="flex items-center gap-2 rounded-full border border-gray-300 py-2 pr-4 pl-6 shadow-md"
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
@@ -62,8 +69,8 @@ const Header = () => {
               clipRule="evenodd"
             />
           </svg>
-          <p>Nana ABC</p>
-        </div>
+          <p className="max-w-20 truncate sm:max-w-32">Nana ABC blablabla</p>
+        </Link>
       </div>
     </header>
   );
