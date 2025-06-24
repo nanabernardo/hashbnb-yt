a7430423aa3613da6b60cb22e4f16fc3b0e16aa3
nanabernardo <nanabernardo@gmail.com>
2025-06-23 22:56:37 -0300

    feat(api): add CORS middleware to allow cross-origin requests

--- back-end/index.js
+++ back-end/index.js
@@ -1,11 +1,13 @@
 import "dotenv/config";
 import express from "express";
 import UserRoutes from "./domains/users/routes.js";
+import cors from "cors";
 
 const app = express();
 const { PORT } = process.env; // = const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(cors());
 app.use("/users", UserRoutes);
 
 app.listen(PORT, () => {
