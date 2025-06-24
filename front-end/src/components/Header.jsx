c7fe24ddec01964392a0ea3b307537b18b78cb74
nanabernardo <nanabernardo@gmail.com>
2025-06-23 22:59:31 -0300

    feat(header): display user name and dynamic navigation based on login state

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Header = () => {
+const Header = ({ user }) => {
   return (
     <header className="shadow-md">
       <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-4 sm:px-8">
@@ -41,7 +41,7 @@ const Header = () => {
         </Link>
 
         <Link
-          to="/login"
+          to={user ? "/account" : "/login"}
           className="flex items-center gap-2 rounded-full border border-gray-300 py-2 pr-4 pl-6 shadow-md"
         >
           <svg
@@ -69,7 +69,12 @@ const Header = () => {
               clipRule="evenodd"
             />
           </svg>
-          <p className="max-w-20 truncate sm:max-w-32">Nana ABC blablabla</p>
+
+          {user ? (
+            <p className="max-w-20 truncate sm:max-w-32">{user.name}</p>
+          ) : (
+            <></>
+          )}
         </Link>
       </div>
     </header>
