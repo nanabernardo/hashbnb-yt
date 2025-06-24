7b87f2721a1e282fb5eed401352cad0d4ff0fff3
nanabernardo <nanabernardo@gmail.com>
2025-06-23 22:57:23 -0300

    feat: create initial Register page with form fields

---
+++ front-end/src/pages/Resgister.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import { Link, Navigate } from "react-router-dom";
+import axios from "axios";
+
+const Register = ({ setUser }) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [redirect, setRedirect] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    /* if (email && password) {
+      try {
+        const { data: userDoc } = await axios.post("/users/login", {
+          email,
+          password,
+        });
+
+        setUser(userDoc);
+        setRedirect(true);
+      } catch (error) {
+        alert(`Deu erro ao logar: ${error.response.data}`);
+      }
+    } else {
+      alert("Você precisa preencher o email e a senha!");
+    }*/
+  };
+
+  if (redirect) return <Navigate to="/" />;
+
+  return (
+    <section className="flex items-center">
+      <div className="mx-auto flex w-full max-w-96 flex-col items-center gap-4">
+        <h1 className="text-3xl font-bold"> Faça seu cadastro</h1>
+
+        <form className="flex w-full flex-col gap-2" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            className="w-full rounded-full border border-gray-300 px-4 py-2"
+            placeholder="Digite seu nome"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="email"
+            className="w-full rounded-full border border-gray-300 px-4 py-2"
+            placeholder="Digite seu e-mail"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            className="w-full rounded-full border border-gray-300 px-4 py-2"
+            placeholder="Digite sua senha"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <button className="bg-primary-400 w-full cursor-pointer rounded-full border border-gray-300 px-4 py-2 font-bold text-white">
+            Registrar
+          </button>
+        </form>
+
+        <p>
+          Já tem uma conta?&nbsp;
+          <Link to="/login" className="font-semibold underline">
+            Logue aqui!
+          </Link>
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Register;
