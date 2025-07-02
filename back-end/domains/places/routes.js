ad781f5e0138a89bf03069d065e1c74e982426c7
nanabernardo <nanabernardo@gmail.com>
2025-07-01 22:35:05 -0300

    feat: Defining the POST Endpoint to save places to the database

--- back-end/domains/places/routes.js
+++ back-end/domains/places/routes.js
@@ -1,5 +1,38 @@
 import { Router } from "express";
+import Place from "./model";
 
 const router = Router;
+
+router.post("/", async (req, res) => {
+  const {
+    title,
+    city,
+    photos,
+    description,
+    extras,
+    price,
+    checkin,
+    checkout,
+    guests,
+  } = req.body;
+
+  try {
+    const newPlaceDoc = await Place.create({
+      owner,
+      title,
+      city,
+      photos,
+      description,
+      extras,
+      price,
+      checkin,
+      checkout,
+      guests,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
+});
 
 export default router;
