aad56eee04d4bae85c5d08fe0ed8f8c8ff4242ed
nanabernardo <nanabernardo@gmail.com>
2025-07-06 22:42:04 -0300

    refactor: rename jwtVerify.js to jwt.js

--- back-end/utils/jwt.js
+++ back-end/utils/jwt.js
@@ -13,11 +13,29 @@ export const JWTVerify = (req) => {
           console.error("Deu algum erro ao verificar com o JWT: ", error);
           reject(error);
         }
-        
+
         resolve(userInfo);
       });
     });
   } else {
     return null;
   }
+};
+
+export const JWTSign = (newUserObj) => {
+  return new Promise((resolve, reject) => {
+    jwt.sign(
+      newUserObj,
+      JWT_SECRET_KEY,
+      { expiresIn: "1d" },
+      (error, token) => {
+        if (error) {
+          console.error("Deu algum erro ao assinar com o JWT: ", error);
+          reject(error);
+        }
+
+        resolve(token);
+      }
+    );
+  });
 };
