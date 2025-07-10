8a411fd2718fb5e8f4f9f4365524b53842024b4f
nanabernardo <nanabernardo@gmail.com>
2025-07-10 20:29:47 -0300

    feat: add image upload route via link

--- back-end/domains/places/routes.js
+++ back-end/domains/places/routes.js
@@ -2,12 +2,13 @@ import { Router } from "express";
 import Place from "./model.js";
 import { JWTVerify } from "../../utils/jwt.js";
 import { connectDb } from "../../config/db.js";
+import { downloadImage } from "../../utils/imageDownloader.js";
 
 const router = Router();
 
 router.post("/", async (req, res) => {
   connectDb();
-  
+
   const {
     title,
     city,
@@ -43,6 +44,12 @@ router.post("/", async (req, res) => {
     console.error(error);
     res.status(500).json("Deu erro ao criaro novo lugar.");
   }
+});
+
+router.post("/upload/link", async (req, res) => {
+  const { link } = req.body;
+
+  await downloadImage(link);
 });
 
 export default router;
