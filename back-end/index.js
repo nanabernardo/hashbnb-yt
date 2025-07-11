a186a0f3aa0b8dc7ff3f8fc079902d7c03e5383b
nanabernardo <nanabernardo@gmail.com>
2025-07-11 18:44:12 -0300

    refactor: restructure backend to separate server and app logic

--- back-end/index.js
+++ back-end/index.js
@@ -1,28 +1,12 @@
 import "dotenv/config";
-import express from "express";
-import UserRoutes from "./domains/users/routes.js";
-import PlaceRoutes from "./domains/places/routes.js";
-import cors from "cors";
-import cookieParser from "cookie-parser";
 import { fileURLToPath } from "url"; //pega url e converte para um caminho de arquivo
 import { dirname } from "node:path";
-
-const app = express();
-const { PORT } = process.env; // = const PORT = process.env.PORT;
+import { app } from "./server.js";
 
 export const __filename = fileURLToPath(import.meta.url);
 export const __dirname = dirname(__filename);
 
-app.use(express.json());
-app.use(cookieParser());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
-app.use("/users", UserRoutes);
-app.use("/places", PlaceRoutes);
+const { PORT } = process.env; // = const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Servidor está rodando na porta ${PORT}`);
---
+++ back-end/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import router from "./routes/index.js";
+
+export const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.use(router);
