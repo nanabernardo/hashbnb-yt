9cca02bb2561938da962ad60460d48ef07539644
nanabernardo <nanabernardo@gmail.com>
2025-07-14 21:23:26 -0300

    "chore: remove dirname.js file"

--- back-end/utils/dirname.js
+++
@@ -1,5 +0,0 @@
-import { fileURLToPath } from "url"; //pega url e converte para um caminho de arquivo
-import { dirname } from "node:path";
-
-export const __filename = fileURLToPath(import.meta.url);
-export const __dirname = dirname(__filename);
