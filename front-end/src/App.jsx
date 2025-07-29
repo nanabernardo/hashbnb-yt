fe07d0c80b282877ebf50239db968e5af26c3cdc
nanabernardo <nanabernardo@gmail.com>
2025-07-29 11:51:52 -0300

    feat: add optional id param to account route for place details

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/account/:subpage/:action?" element={<Account />} />
+          <Route path="/account/:subpage/:action?/:id?" element={<Account />} />
         </Routes>
       </BrowserRouter>
     </UserContextProvider>
