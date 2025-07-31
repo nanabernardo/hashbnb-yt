04c39aaa303c3886f603a224154ff8c34b67e802
nanabernardo <nanabernardo@gmail.com>
2025-07-31 18:42:26 -0300

    feat: fetch places from API and render with Item component

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -1,21 +1,28 @@
-import React from 'react'
-import Item from '../components/Item'
+import React, { useEffect, useState } from "react";
+import Item from "../components/Item";
+import axios from "axios";
 
 const Home = () => {
+  const [places, setPlaces] = useState([]);
+
+  useEffect(() => {
+    const axiosGet = async () => {
+      const { data } = await axios.get("/places");
+      setPlaces(data);
+    };
+
+    axiosGet();
+  }, []);
+
   return (
     <section>
-    <div className="gap-8 mx-auto grid max-w-7xl grid-cols-[repeat(auto-fit,minmax(225px,1fr))] p-8">
-      <Item />
-      <Item />
-      <Item />
-      <Item />
-      <Item />
-      <Item />
-      <Item />
-      <Item />
-    </div>
-  </section>
-  )
-}
+      <div className="mx-auto grid max-w-7xl grid-cols-[repeat(auto-fit,minmax(225px,1fr))] gap-8 p-8">
+        {places.map((place) => (
+          <Item {...{ place }} key={place._id} />
+        ))}
+      </div>
+    </section>
+  );
+};
 
-export default Home
+export default Home;
