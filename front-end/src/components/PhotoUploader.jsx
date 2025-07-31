897f8088d81fecc04ff070fb5a11d131a8a8fdb7
nanabernardo <nanabernardo@gmail.com>
2025-07-30 22:17:50 -0300

    feat(photo-uploader): add action buttons overlay on uploaded photos

--- front-end/src/components/PhotoUploader.jsx
+++ front-end/src/components/PhotoUploader.jsx
@@ -47,6 +47,7 @@ const PhotoUploader = ({ photoLink, setPhotoLink, setPhotos, photos }) => {
       <label htmlFor="photos" className="ml-2 text-2xl font-bold">
         Fotos
       </label>
+
       <div className="flex gap-2">
         <input
           type="text"
@@ -66,12 +67,50 @@ const PhotoUploader = ({ photoLink, setPhotoLink, setPhotos, photos }) => {
 
       <div className="mt-2 grid grid-cols-5 gap-4">
         {photos.map((photo) => (
-          <img
-            className="aspect-square rounded-2xl object-cover"
-            key={photo}
-            src={`${photo}`}
-            alt="Imagens do lugar"
-          />
+          <div className="relative">
+            <img
+              className="aspect-square rounded-2xl object-cover"
+              key={photo}
+              src={`${photo}`}
+              alt="Imagens do lugar"
+            />
+
+            <div className="absolute right-2 bottom-2 flex gap-1">
+              <div className="hover:bg-primary-400 cursor-pointer rounded-full bg-gray-100 p-2 opacity-75 transition hover:text-white">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
+                  />
+                </svg>
+              </div>
+
+              <div className="hover:bg-primary-400 cursor-pointer rounded-full bg-gray-100 p-2 opacity-75 transition hover:text-white">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
         ))}
 
         <label
