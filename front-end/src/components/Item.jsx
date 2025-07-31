2f2aa00664d0e446939cb20404d1fbf20b8b34b7
nanabernardo <nanabernardo@gmail.com>
2025-07-31 18:42:12 -0300

    refactor: make Item component dynamic using props

--- front-end/src/components/Item.jsx
+++ front-end/src/components/Item.jsx
@@ -1,27 +1,21 @@
 import React from "react";
 
-const Item = () => {
+const Item = ({ place }) => {
   return (
     <a href="/" className="flex flex-col gap-2">
       <img
-        src="https://images.unsplash.com/photo-1578683010236-d716f9a3f461?"
+        src={place.photos[0]}
         alt="Imagem da acomodação"
         className="aspect-square rounded-2xl object-cover"
       />
 
       <div>
-        <h3 className="text-xl font-semibold">Serra Negra - SP</h3>
-        <p className="truncate text-gray-600">
-          Rodeada por paisagens montanhosas e ar puro, esta casa de campo
-          oferece uma estadia tranquila e confortável. Possui 4 quartos, sala
-          com lareira, cozinha rústica, varanda com redes e um pomar com frutas
-          da estação. O quintal amplo é perfeito para churrascos e tardes ao ar
-          livre. Ótima escolha para famílias, retiros ou escapadas românticas.
-        </p>
+        <h3 className="text-xl font-semibold">{place.city}</h3>
+        <p className="truncate text-gray-600">{place.description}</p>
       </div>
 
       <p>
-        <span className="font-semibold">R$ 550</span> por noite
+        <span className="font-semibold">R$ {place.price}</span> por noite
       </p>
     </a>
   );
