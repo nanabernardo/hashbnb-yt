43644a0d376fbd756ec9790c610cfaef46c9fcf9
nanabernardo <nanabernardo@gmail.com>
2025-08-01 18:18:31 -0300

    feat(place): add "show more images" button overlay on photo grid

--- front-end/src/pages/Place.jsx
+++ front-end/src/pages/Place.jsx
@@ -51,7 +51,7 @@ const Place = () => {
             <p>{place.city}</p>
           </div>
 
-          <div className="grid aspect-[3/2] grid-cols-[2fr_1fr] grid-rows-2 gap-4 overflow-hidden rounded-2xl">
+          <div className="relative grid aspect-[3/2] grid-cols-[2fr_1fr] grid-rows-2 gap-4 overflow-hidden rounded-2xl">
             {place.photos
               .filter((photo, index) => index < 3)
               .map((photo, index) => (
@@ -62,6 +62,25 @@ const Place = () => {
                   alt="Imagem da Acomodação"
                 />
               ))}
+
+            <div className="absolute right-2 bottom-2 flex cursor-pointer gap-2 rounded-xl border border-black bg-white px-2 py-1 transition hover:scale-105">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M13.5 16.875h3.375m0 0h3.375m-3.375 0V13.5m0 3.375v3.375M6 10.5h2.25a2.25 2.25 0 0 0 2.25-2.25V6a2.25 2.25 0 0 0-2.25-2.25H6A2.25 2.25 0 0 0 3.75 6v2.25A2.25 2.25 0 0 0 6 10.5Zm0 9.75h2.25A2.25 2.25 0 0 0 10.5 18v-2.25a2.25 2.25 0 0 0-2.25-2.25H6a2.25 2.25 0 0 0-2.25 2.25V18A2.25 2.25 0 0 0 6 20.25Zm9.75-9.75H18a2.25 2.25 0 0 0 2.25-2.25V6A2.25 2.25 0 0 0 18 3.75h-2.25A2.25 2.25 0 0 0 13.5 6v2.25a2.25 2.25 0 0 0 2.25 2.25Z"
+                />
+              </svg>
+
+              <p>Mostrar mais imagens</p>
+            </div>
           </div>
         </div>
       </div>
