c8a32483ff88bd7a6662f6538b26f24c39a8a7ce
nanabernardo <nanabernardo@gmail.com>
2025-08-09 10:01:59 -0300

    feat(booking): add POST route to create new booking

--- back-end/domains/booking/routes.js
+++ back-end/domains/booking/routes.js
@@ -1,6 +1,31 @@
 import { Router } from "express";
 import Booking from "./model.js";
+import { connectDb } from "../../config/db.js";
 
 const router = Router();
+
+router.post("/", async (req, res) => {
+  connectDb();
+  
+  const { place, user, price, total, checkin, checkout, guests, nights } =
+    req.body;
+  try {
+    const newBookingDoc = await Booking.create({
+      place,
+      user,
+      price,
+      total,
+      checkin,
+      checkout,
+      guests,
+      nights,
+    });
+
+    res.json(newBookingDoc);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json("Deu erro ao criar a Reserva.");
+  }
+});
 
 export default router;
