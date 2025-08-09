4a97eb0953841072d41eb5cfea4f552bcc482aa9
nanabernardo <nanabernardo@gmail.com>
2025-08-09 12:23:04 -0300

    feat(bookings): implement GET user bookings route with token validation; maintain POST route for booking creation

--- back-end/domains/booking/routes.js
+++ back-end/domains/booking/routes.js
@@ -1,12 +1,35 @@
 import { Router } from "express";
 import Booking from "./model.js";
 import { connectDb } from "../../config/db.js";
+import { JWTVerify } from "../../utils/jwt.js";
 
 const router = Router();
 
+router.get("/", async (req, res) => {
+  connectDb();
+
+  try {
+    const { _id: id } = await JWTVerify(req);
+
+    try {
+      const bookingDocs = await Booking.find({ user: id });
+
+      res.json(bookingDocs);
+    } catch (error) {
+      console.error(error);
+      res
+        .status(500)
+        .json("Deu erro ao buscar todas as Reservas daquele usuário.");
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json("Deu erro ao validar o token do usuário.");
+  }
+});
+
 router.post("/", async (req, res) => {
   connectDb();
-  
+
   const { place, user, price, total, checkin, checkout, guests, nights } =
     req.body;
   try {
